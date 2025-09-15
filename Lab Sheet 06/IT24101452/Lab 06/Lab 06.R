88c7d2ce70410d3830eec4f4df71951cf7b5299c
IT24101452 <It24101452@my.sliit.lk>
2025-09-15 09:16:32 +0530

    Add files via upload

---
+++ Lab Sheet 06/IT24101452/Lab 06/Lab 06.R
@@ -0,0 +1,19 @@
+getwd()
+setwd("C://Users//chami//OneDrive//Desktop//IT24101452")
+
+##Q1
+##Random variable has binomial distribution with n=50 and p=0.85
+
+##Q2
+##(X>=47)
+pbinom(47,50,0.85,lower.tail=FALSE)
+
+
+##Q1
+##Average calls per day
+
+##Q2
+##Random variable x has a poisson distribution with lambda=12
+
+##Q3
+dpois(15,12)
