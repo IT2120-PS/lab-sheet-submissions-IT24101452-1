2e6bbd2f3af64f333b57546625caf108dbd115f0
IT24101452 <It24101452@my.sliit.lk>
2025-09-16 15:18:03 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24101452/Lab 07.R
@@ -0,0 +1,16 @@
+getwd()
+setwd("C://Users//chami//OneDrive//Desktop")
+
+##Q1
+punif(10,min=0,max=40,lower.tail = TRUE)-punif(25,min=0,max=40,lower.tail = TRUE)
+
+
+##Q2
+pexp(2,rate = 0.33,lower.tail = TRUE)
+
+
+##Q3
+pnorm(130,mean = 100,sd=15,lower.tail = TRUE)
+
+##
+qnorm(0.95,mean=100,sd=15,lower.tail = TRUE)
